5f4f2074bfb83cee901d26e4d02c9342ab72d4b4
aggre <hiroyuki.aggre@gmail.com>
2020-10-27 11:02:05 +0900

    rename args and add test cases

--- contracts/Deploy.sol
+++ contracts/Deploy.sol
@@ -6,10 +6,10 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 contract Deploy {
 	TokenGeyser private tokenGeyser;
 
-	constructor(address staking, address reword, uint256 maxUnlockSchedules, uint256 startBonus, uint256 bonusPeriodSec, uint256 initialSharesPerToken) public {
-		IERC20 iuniv2 = IERC20(staking);
-		IERC20 idev = IERC20(reword);
-		tokenGeyser = new TokenGeyser(iuniv2, idev, maxUnlockSchedules, startBonus, bonusPeriodSec, initialSharesPerToken);
+	constructor(address staking, address distribution, uint256 maxUnlockSchedules, uint256 startBonus, uint256 bonusPeriodSec, uint256 initialSharesPerToken) public {
+		IERC20 stakingToken = IERC20(staking);
+		IERC20 distributionToken = IERC20(distribution);
+		tokenGeyser = new TokenGeyser(stakingToken, distributionToken, maxUnlockSchedules, startBonus, bonusPeriodSec, initialSharesPerToken);
 		tokenGeyser.transferOwnership(msg.sender);
 	}
 
